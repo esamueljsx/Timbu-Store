0866cc51e2bab6079c68346957740ef88d5a94c9
samandcode <chijiokeemechebe@gmail.com>
2024-07-09 11:53:35 +0100

    Confirmation page mobile fix

--- src/app/checkout/confirmed/page.js
+++ src/app/checkout/confirmed/page.js
@@ -7,8 +7,8 @@ const OrderConfirmationPage = () => {
         <PageWrapper>
             <section className="py-16 antialiased">
                 <div className="max-w-7xl mx-auto px-4 flex flex-col items-center justify-center gap-6 text-center">
-                    <Icon type="check" size={48} />
-                    <h1 className="text-5xl font-bold text-white/85">Order Confirmed!</h1>
+                    <Icon type="check" size={86} />
+                    <h1 className="lg:text-5xl text-4xl font-bold text-white/85">Order Confirmed!</h1>
                     <p className="text-lg text-white">
                         Thank you for your purchase. Your order has been placed successfully.
                     </p>
